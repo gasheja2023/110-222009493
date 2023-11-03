e3bf92a1fdfd586d5a3661ad192d2624dfea38f4
gasheja2023 <149682099+gasheja2023@users.noreply.github.com>
2023-11-03 11:24:11 +0200

    Add files via upload

---
+++ teacher.java
@@ -0,0 +1,14 @@
+class students{
+    public static void main(String[]args){
+        if (ages ==50){
+        Ststem.out.println("oldest");
+        }
+            else if(ages<50){
+                System.out.println("young");
+            }
+            else{
+                System out.println("younger");
+            }
+        }    
+        }
+    
