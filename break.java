8cd417a716d5e69561a09695893d024846c0fbf3
gasheja2023 <149682099+gasheja2023@users.noreply.github.com>
2023-11-03 11:08:03 +0200

    Add files via upload

---
+++ break.java
@@ -0,0 +1,10 @@
+class gk{
+    public static void main(String args[]){
+        for(int a=1;a<30;a++){
+            if(a==20){
+                break;
+            }
+            System.out.println(a);
+        }
+    }
+}
---
+++ home.java
@@ -0,0 +1,8 @@
+ class home {
+ public static void main(String arg[]){
+    int i=1;
+    while(i<5){
+        System.out.println("i");
+    }
+ }   
+}
