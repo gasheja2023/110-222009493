c719909623104ff65d5d0a56cef7ea75f9003677
gasheja2023 <149682099+gasheja2023@users.noreply.github.com>
2023-11-08 19:18:53 +0200

    Add files via upload

---
+++ class1.java
@@ -0,0 +1,9 @@
+class arraays1{
+    public static void main(String args[]){
+    String names[] ={"leo","lene","iranzi"};
+    for(int i=0; i<3; i++){
+
+    System.out.println(names[i]);
+    }
+}
+}
