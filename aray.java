0f882bb68ccc1e61053c6d2b70cbe3267ec37281
gasheja2023 <149682099+gasheja2023@users.noreply.github.com>
2023-11-08 07:45:05 +0200

    Add files via upload

---
+++ aray.java
@@ -0,0 +1,8 @@
+class array{
+    public static void main(String args[]){
+        String names[]={"augustin","denyse","delphine"};
+for(int i=1;i<3;i++){
+    System.out.println(names[i]);
+}
+    }
+}
